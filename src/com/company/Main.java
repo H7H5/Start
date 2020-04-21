2250a98e3b0e62a42a3e0b14b080e8e9bf707565
admin <Serdyk87@ukr.net>
2020-04-21 12:22:36 +0300

    Lesson1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
-public class Main {
+import javax.swing.*;
+
+public class Main extends JPanel { //наследуемся от класса JPanel
+
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
 
     public static void main(String[] args) {
-	    System.out.println("Hello World");
+        JFrame frame = new JFrame("Гонки");   //создаем объект frame
+        frame.setSize(WIDTH, HEIGHT);       //устанавливаем размеры
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);// закрывает окно
+        frame.setResizable(false);                //запрет на изменение размера окна
+        frame.setLocationRelativeTo(null);        // окно будет по центру
+        frame.add(new Space());                   // добавляем объект Space
+        frame.setVisible(true);                   // делаем фрейм видимым
     }
 }
---
+++ src/com/company/Space.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import javax.swing.*;
+
+/**
+ * Created by admin on 21.04.2020.
+ */
+public class Space extends JPanel {
+}
